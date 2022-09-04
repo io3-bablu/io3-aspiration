533cb3cfac00670509361fafd3982c31f1aeaddb
bobby <bs40017@gmail>
2022-09-04 17:25:13 +0530

    "20220904 java script "

---
+++ practiceset/javascript/index.js
@@ -0,0 +1,22 @@
+var first_name = 10;
+var last_name = 2;
+console.log("hello this is bobby", first_name * last_name);
+
+const studentName = "Ankur";
+
+const mathMarks = 55;
+const EngMarks = 70;
+const sceincemarks = 45;
+
+console.log("2022-09-04 --> 1 -->");
+
+// My name is Ankur Maurya. I got marks in math is 55, in science is 45 and in English is 70
+
+console.log(
+  "my name is",
+  studentName,
+  "maurya. I got marks in math is",
+  mathMarks,
+  "in science is 45 and in English is",
+  EngMarks
+);
