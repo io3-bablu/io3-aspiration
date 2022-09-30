8cd140a92ab798dc0703df90f878ffb3149defbd
bobby <bs40017@gmail>
2022-09-30 10:45:13 +0530

    2022-09-30-practice-events

--- practiceset/eventsInJs/index.js
+++ practiceset/eventsInJs/index.js
@@ -2,7 +2,7 @@
 const callingFunction = () => {
   alert("most common way to wright function");
 };
-// 3rd way to wright function ==> its not work if we use same reference for multiple events call
+// 3rd way to wright function ==> its not work if we use same reference for multiple event call
 const thirdWay = document.getElementById("button3");
 thirdWay.onclick = () => {
   alert("3rd and most common way to wright a function again");
@@ -10,7 +10,7 @@ thirdWay.onclick = () => {
 thirdWay.onclick = () => {
   console.log("3rd and most common way to wright a function again");
 };
-//4th way to wright function ==> u can use same reference for multiple events call
+//4th way to wright function ==> u can use same reference for multiple event call
 const fourthWay = document.querySelector("#button4");
 fourthWay.addEventListener("click", () => {
   alert("i love this way of writing function");
